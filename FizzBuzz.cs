9949f7f23fea8ec74e6622c2c1577e2d215bc56c
Tadiya Brown <tadiyabrown2000@gmail.com>
2023-05-10 17:56:23 -0500

    FizzBuzz assignment done

---
+++ FizzBuzz.cs
@@ -0,0 +1,28 @@
+//Write a public method that accepts a number
+
+//when the number is divisible by 3 return the word fizz
+
+//when the number is divisible by 5 return the word buzz
+
+//when the number is divisible by both, return the word fizzbuzz
+
+public string FizzBuzz(int number)
+
+{
+    var answer = "";
+
+    if(number % 3 == 0)
+    {
+        answer = "Fizz";
+
+    }
+
+    if( number % 5 == 0)
+    {
+        answer += "Buzz";
+    }
+
+    return answer;
+
+
+}
